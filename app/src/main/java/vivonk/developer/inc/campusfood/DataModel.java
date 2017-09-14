aa52a36301d4d553c797954032bbfbe8cd529609
kundank78 <kkundan.iitr@gmail.com>
2017-09-14 17:19:19 +0530

    data model class completed

--- app/src/main/java/vivonk/developer/inc/campusfood/DataModel.java
+++ app/src/main/java/vivonk/developer/inc/campusfood/DataModel.java
@@ -1,6 +1,34 @@
 package vivonk.developer.inc.campusfood;
 
 
-
 public class DataModel {
+
+    String name;
+    String type;
+    String version_number;
+    String feature;
+
+    public DataModel(String name, String type, String version_number ) {
+        this.name=name;
+        this.type=type;
+        this.version_number=version_number;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVersion_number() {
+        return version_number;
+    }
+
+    public String getFeature() {
+        return feature;
+    }
+
 }
