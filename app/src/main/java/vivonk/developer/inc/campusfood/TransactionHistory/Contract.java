5621cc72c80888408a0dc48e7448deb84bcdc079
kundank78 <kkundan.iitr@gmail.com>
2017-09-14 17:21:38 +0530

    transaction history

---
+++ app/src/main/java/vivonk/developer/inc/campusfood/TransactionHistory/Contract.java
@@ -0,0 +1,54 @@
+package vivonk.developer.inc.campusfood.TransactionHistory;
+
+
+public class Contract {
+    int _id;
+    String _name;
+    String _phone_number;
+
+    // Empty constructor
+    public Contract(){
+
+    }
+    // constructor
+    public Contract(int id, String name, String _phone_number){
+        this._id = id;
+        this._name = name;
+        this._phone_number = _phone_number;
+    }
+
+    // constructor
+    public Contract(String name, String _phone_number){
+        this._name = name;
+        this._phone_number = _phone_number;
+    }
+    // getting ID
+    public int getID(){
+        return this._id;
+    }
+
+    // setting id
+    public void setID(int id){
+        this._id = id;
+    }
+
+    // getting name
+    public String getName(){
+        return this._name;
+    }
+
+    // setting name
+    public void setName(String name){
+        this._name = name;
+    }
+
+    // getting phone number
+    public String getPhoneNumber(){
+        return this._phone_number;
+    }
+
+    // setting phone number
+    public void setPhoneNumber(String phone_number){
+        this._phone_number = phone_number;
+    }
+}
